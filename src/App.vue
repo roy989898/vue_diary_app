7e156097a73c87ef047ce79c6c7e613122821b1b
roy989898 <roy.leung@gravitas.com.hk>
2018-07-31 12:19:57 +0800

    center

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
-  <div id="app">
-    <div class="alert alert-primary" role="alert">
-      My Diary App
+    <div id="app">
+        <div class="alert alert-primary" role="alert" style="text-align: center">
+            My Diary App
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            HelloWorld
+        }
+    }
 </script>
 
 <style>
