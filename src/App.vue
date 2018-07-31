49f82eb83c69abf6a2420d51badf79e3654d16a7
roy989898 <roy989898@gmail.com>
2018-07-31 20:36:29 +0800

    add footer and header

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
     <div id="app">
+        <app-header></app-header>
         <div class="alert alert-primary" role="alert" style="text-align: center">
             My Diary App
         </div>
+        <app-footer></app-footer>
     </div>
 </template>
 
 <script>
     import HelloWorld from './components/HelloWorld.vue'
+    import AppHeader from './components/BasicComponent/Header.vue'
+    import AppFooter from './components/BasicComponent/Footer.vue'
+
 
     export default {
         name: 'app',
         components: {
-            HelloWorld
+            HelloWorld,
+            'app-header': AppHeader,
+            'app-footer': AppFooter,
         }
     }
 </script>
--- src/components/BasicComponent/Header.vue
+++ src/components/BasicComponent/Header.vue
@@ -1,4 +1,5 @@
 <template>
+    <div>I am a Header</div>
     
 </template>
 
