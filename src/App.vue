98455a153000854c078db5feef898ff20fa056d4
roy989898 <roy.leung@gravitas.com.hk>
2018-07-31 12:17:32 +0800

    add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="alert alert-primary" role="alert">
+      My Diary App
+    </div>
   </div>
 </template>
 
@@ -17,12 +18,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
