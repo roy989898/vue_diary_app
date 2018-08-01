933c44d4512b3688729d02d824d934377242be4c
roy989898 <roy.leung@gravitas.com.hk>
2018-08-01 15:43:21 +0800

    add temp diary data

---
+++ src/components/TemDiarysStore.js
@@ -0,0 +1,11 @@
+import DiaryClass from "../Class/DiaryClass";
+
+const diarys = [
+    new DiaryClass('A', new Date(2018.12, 3), "HIHIH"),
+    new DiaryClass('B', new Date(2018.12, 5), "YO"),
+    new DiaryClass('C', new Date(2018.12, 6), "435345"),
+    new DiaryClass('D', new Date(2018.12, 7), "Car"),
+    new DiaryClass('A', new Date(2018.12, 8), "HAYHAYHAYHAYHAYHAY"),
+];
+
+export default diarys
