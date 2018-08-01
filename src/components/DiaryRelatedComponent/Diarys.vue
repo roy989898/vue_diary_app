01708947e2199798eae3c5df9ceb0f48f5810612
roy989898 <roy.leung@gravitas.com.hk>
2018-08-01 15:59:31 +0800

    improve show the diarys

--- src/components/DiaryRelatedComponent/Diarys.vue
+++ src/components/DiaryRelatedComponent/Diarys.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
-        <app-diary-box v-for="diary in diarys" v-bind:date="diary.date" v-bind:content="diary.content"
-                       v-bind:title="diary.title" v-bind:key="diary.date+diary.content+diary.title"></app-diary-box>
+
+        <div class="row">
+            <app-diary-box v-for="diary in diarys" v-bind:date="diary.date" v-bind:content="diary.content"
+                           v-bind:title="diary.title" v-bind:key="diary.date+diary.content+diary.title"></app-diary-box>
+        </div>
+
     </div>
 
 </template>
