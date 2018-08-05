3d97f49577a0d2388f61f7c5fea4128f02aa3e86
roy989898 <roy989898@gmail.com>
2018-08-05 15:00:01 +0800

    can show the content of the diary in the detail page

--- src/components/DiaryRelatedComponent/DiaryEdit.vue
+++ src/components/DiaryRelatedComponent/DiaryEdit.vue
@@ -1,8 +1,39 @@
 <template>
     <div class="col-sm">
         <!--Diary edit {{diaryId}}-->
-        {{diary}}
-        
+        <!--{{diary}}-->
+
+        <form>
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input class="form-control" id="title"
+                       placeholder="Enter Title"
+                       v-bind:value="diary.title">
+            </div>
+            <!--v-bind:value="diary.date|dateFormat"-->
+            <div class="form-group">
+                <label for="title">Date</label>
+                <input class="form-control" id="date"
+                       type="date"
+                       placeholder="Enter Date"
+                       v-bind:value="diary.date|dateFormat"
+                >
+            </div>
+
+            <div class="form-group">
+                <label for="title">Content</label>
+                <!-- <input class="form-control" id="content"
+                        type="date"
+                        placeholder="Enter Date"
+                        v-bind:value="diary.date|dateFormat"
+                 >-->
+
+                <textarea name="Text1" cols="40" rows="5" class="form-control" id="content"
+                          v-bind:value="diary.content"
+                ></textarea>
+            </div>
+        </form>
+
     </div>
 
 </template>
